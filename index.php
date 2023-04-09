c3ddb161dd8b9a4f1853dcca64955bde5ad19297
Artem2710 <38757512+Artem2710@users.noreply.github.com>
2023-04-09 18:51:01 +0300

    asd

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 12121233333344;
+echo 12121;
+
+# Принимаем запрос
+$data = json_decode(file_get_contents('php://input'), TRUE);
+file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+
+
+$file = "file.txt"; // указываем имя файла
+$content = file_get_contents($file); // читаем содержимое файла
+
+print_r($content);
