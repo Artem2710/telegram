ae06190cf52a4255998078cf8989b5c78c9fee2c
Artem2710 <38757512+Artem2710@users.noreply.github.com>
2023-04-09 18:54:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 echo 12121;
 
+$command = "chmod 777 " . 'file.txt';
+exec($command);
+
 # Принимаем запрос
 $data = json_decode(file_get_contents('php://input'), TRUE);
 file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
