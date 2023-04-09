1c36206825dff401bdb2f04bf61105956e0cc60d
Artem2710 <38757512+Artem2710@users.noreply.github.com>
2023-04-09 19:01:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,7 @@
 
 echo 12121;
 
-
-
 # Принимаем запрос
 $data = json_decode(file_get_contents('php://input'), TRUE);
 
-
-$handle = fopen('file.txt', 'w');
-
-// write the data to the file
-fwrite($handle, $data);
-
-// close the file handle
-fclose($handle);
-
-
-// file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
-
-
-$file = "file.txt"; // указываем имя файла
-$content = file_get_contents($file); // читаем содержимое файла
-
-print_r($content);
+print_r($data);
